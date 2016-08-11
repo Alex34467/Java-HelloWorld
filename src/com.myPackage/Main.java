902d25db873b416f9ee98b5d2f3cc743e645fe09
Alex34467 <borisov.alexey0@gmail.com>
2016-08-11 18:07:31 +0300

    Addded Main class.

---
+++ src/com.myPackage/Main.java
@@ -0,0 +1,12 @@
+package com.myPackage;
+
+ // Main class.
+public class Main
+{
+    // Main method.
+    public static void main(String[] args)
+    {
+        // Print: Hello, world!
+        System.out.println("Hello, world!");
+    }
+}
